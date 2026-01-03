5d561ada0c1eced1c54b19e151620db8b9b6a3e9
Ivanaez <ivan.evtimov11@gmail.com>
2026-01-03 01:03:55 +0100

    Implement responsive hamburger navigation

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const message = document.getElementById("message");
 btn.addEventListener("click", function () {
     message.textContent = "Вие успешно добавихте данните. Благодарим!";
 });
+// Hamburger button
+const menuToggle = document.querySelector('.menu-toggle');
+
+// Side menu
+const sideMenu = document.querySelector('.side-menu');
+
+// Toggle menu on click
+menuToggle.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+});
