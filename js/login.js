4d5c626f41d295774430736aa7aea12103f5edfb
Ivanaez <ivan.evtimov11@gmail.com>
2026-02-13 21:13:15 +0100

    Finishing the Login Page

--- js/login.js
+++ js/login.js
@@ -2,22 +2,48 @@
                        // Wait for DOM to load
 document.addEventListener("DOMContentLoaded", () => {
 
+
+
+
                    // Select elements from HTML
   const passwordInput = document.querySelector("#password");
   const passwordError = document.querySelector('.password-error');
   const form = document.querySelector(".login-container");
   const togglePasswordBtn = document.querySelector(".toggle-password");
   const togglePasswordIcon = togglePasswordBtn.querySelector("i");
+  const loginBtn = document.querySelector(".login-btn");
+  const  guestBtn = document.querySelector(".link-btn.guest");
 
-// Show / hide eye button depending on input value
+
+
+
+
+
+                 //control login + eye state
   passwordInput.addEventListener("input", () => {
     const passwordValue = passwordInput.value;
 
+    // Hide error when password becomes valid
+   passwordError.classList.add("hidden");
+
+       // Login button disabled only when length is 1..7
+ if (passwordValue.length === 0) {
+  loginBtn.disabled = false;
+} else if (passwordValue.length < 8) {
+  loginBtn.disabled = true;
+} else {
+  loginBtn.disabled = false;
+}
+
+           
+
+         // Show / hide eye button 
     if (passwordValue.length > 0) {
   togglePasswordBtn.classList.remove("eye-icon-hidden");
 } else {
   togglePasswordBtn.classList.add("eye-icon-hidden");
 }
+
   });
 
             // Toggle show / hide password on button click
@@ -34,6 +60,8 @@ togglePasswordBtn.addEventListener("click", () => {
     togglePasswordIcon.classList.remove("fa-eye");
     togglePasswordIcon.classList.add("fa-eye-slash");
   }
+
+   
 });
 
                   // Password validation 
@@ -67,17 +95,24 @@ function validatePassword(passwordValue) {
     });
 
 
-// Final validation when the user clicks the Login button
+                    // Final validation when the user clicks the Login button
      form.addEventListener("submit", (event) => {
     const passwordValue = passwordInput.value;
     const isValid = validatePassword(passwordValue);
 
-          // Show validation rules to the user
+                       // Show validation rules to the user
     if (!isValid) {
       event.preventDefault();
 
-      passwordError.textContent =
-        "Password must contain at least one uppercase letter, one lowercase letter, one number and be 8–15 characters long.";
+      passwordError.textContent = `
+Password must contain:
+• One uppercase letter
+• One lowercase letter
+• One number
+• 8–15 characters
+`;
+        
+
 
       passwordError.classList.remove("hidden");
     }
